bb42ffd6ce99a67d6e70ae00d0cb6f1779ca6385
hex86 <hadi.af961@gmail.com>
2020-04-28 23:38:20 +0300

    update + fix

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias mkdir='mkdir -pv'
+alias l="ls -GAF"
 alias ll='ls -lG'
 alias la='ls -AG'
 alias lla='ls -GAl'
---
+++ .zsh/env.zsh
@@ -0,0 +1,4 @@
+export PATH=$HOME/Sources/node-v13.13.0-darwin-x64/bin:$PATH
+export PATH=$HOME/Sources/mysql-8.0.19-macos10.15-x86_64/bin:$PATH
+export PATH=$HOME/Sources/aria2-1.35.0/bin:$PATH
+export DOTNET_CLI_TELEMETRY_OPTOUT=true
